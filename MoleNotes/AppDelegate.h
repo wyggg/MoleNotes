f493e19f3b7ac82995f07705424f8c93db2f4692
wugang <773678819@qq.com>
2017-03-04 22:51:45 +0800

    Initial Commit

---
+++ MoleNotes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoleNotes
+//
+//  Created by wugang on 17/3/4.
+//  Copyright © 2017年 wugang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MoleNotes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MoleNotes
+//
+//  Created by wugang on 17/3/4.
+//  Copyright © 2017年 wugang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
